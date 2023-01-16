2b65f400fe07ea18b0bf4483996a649ed2fdf4c1
matheusaugsschmitz <augs.math@gmail.com>
2023-01-16 20:24:20 -0300

    Creating repository to manage stack usage

---
+++ src/main/java/com/tmg/codingchallenge/stackchallenge/repository/StackRepository.java
@@ -0,0 +1,18 @@
+package com.tmg.codingchallenge.stackchallenge.repository;
+
+import com.tmg.codingchallenge.stackchallenge.model.CustomStack;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StackRepository {
+
+    private static final CustomStack<String> stack = new CustomStack<>();
+
+    public String popTopItem() {
+        return stack.pop();
+    }
+
+    public void pushItem(String newItem) {
+        stack.pushItem(newItem);
+    }
+}
---
+++ src/test/java/com/tmg/codingchallenge/unittest/stackchallenge/repository/StackRepositoryTest.java
@@ -0,0 +1,58 @@
+package com.tmg.codingchallenge.unittest.stackchallenge.repository;
+
+import com.tmg.codingchallenge.stackchallenge.model.CustomStack;
+import com.tmg.codingchallenge.stackchallenge.repository.StackRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.lang.reflect.Field;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+class StackRepositoryTest {
+
+    @InjectMocks
+    private StackRepository stackRepository;
+
+    @Test
+    void pushItem_withSuccess() {
+        // Arrange
+        String stackItem = "Lada Niva";
+
+        // Act
+        stackRepository.pushItem(stackItem);
+
+        // Assert
+        CustomStack<String> stack = getStack();
+        assertEquals(1, stack.getSize());
+        assertEquals(stackItem, stack.pop());
+    }
+
+    @Test
+    void popItem_withSuccess() {
+        // Arrange
+        String stackItem = "Lada Niva";
+        CustomStack<String> stack = getStack();
+        stack.pushItem(stackItem);
+
+        // Act
+        String response = stackRepository.popTopItem();
+
+        // Assert
+        assertEquals(0, stack.getSize());
+        assertEquals(stackItem, response);
+    }
+
+    private CustomStack<String> getStack() {
+        try {
+            Field privateField = StackRepository.class.getDeclaredField("stack");
+            privateField.setAccessible(true);
+            return (CustomStack<String>) privateField.get(stackRepository);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
